cf13360bc4396179081d5768c27b567675c4235f
Timofey.Kirilichev <Timofey.Kirilichev@T04-369-W15>
2024-09-16 17:32:52 +0500

    Добавил программа вычесления двух целых чисел

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace po
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введие первое число");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введие второе число");
+            int b = int.Parse(Console.ReadLine());
+          
+            Console.WriteLine("Cумма равна=" + (a+b));
+
+            Console.ReadKey();
+        }
+    }
+
+}
