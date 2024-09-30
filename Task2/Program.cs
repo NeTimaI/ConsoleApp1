ee66fb70805b1c886090f9e2c5b42cd7ea5bc156
Timofey_Kirilichev <Timof@DESKTOP-CCFM2G9>
2024-09-30 17:06:50 +0500

    Рассчитана площадь колца по внешнему и внутреннему диаметру. Вариант 10

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите внешний диаметр:");
+            Double A = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите внутренний диаметр:");
+            Double B = Convert.ToDouble(Console.ReadLine());
+            double C = (Math.PI / 4) * ((A * A) - (B * B));
+            Console.WriteLine("Площадь(S) =" + Math.Round(C, 2));
+            Console.ReadKey();
+        }
+    }
+}
