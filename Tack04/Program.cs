aba79681d282a185f7c5e70d8add4f521ab3deff
Timofey_Kirilichev <Timof@DESKTOP-CCFM2G9>
2024-10-07 16:56:28 +0500

    Вариант 10. Математическа формула

---
+++ Tack04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tack04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите дейёствительное число x:");
+            var x = double.Parse(Console.ReadLine());
+            var y = MyFunction(x);
+            Console.WriteLine($"f(x) = {y}");
+            Console.ReadKey();
+        }
+        static double MyFunction(double x)
+        {
+            return Math.Sqrt((x * x - 1) / (x * x + 1)) + 1;
+        }
+    }
+    }
+}
