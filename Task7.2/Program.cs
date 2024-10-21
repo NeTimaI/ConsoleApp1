14d29060c10f5c0ebfdf4c8b958d9c3eb0e719eb
Timofey_Kirilichev <Timof@DESKTOP-CCFM2G9>
2024-10-21 17:11:54 +0500

    Задание 7.2 выполненно. Точки на фигуре

---
+++ Task7.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x:");
+
+            var x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите y");
+            
+            var y = int.Parse(Console.ReadLine());
+
+            if (Cheak(x,y))
+            {
+                Console.WriteLine("Точки принадлежат фигуре");
+            }
+            else { Console.WriteLine("Точки не принадлжеат фигуре"); }
+            Console.ReadKey();
+        }
+
+        static bool Cheak( int x, int y )
+        {
+            return (x >= 2 && y >= 0) || (x >= 1 && y <= -1);
+        }
+    }
+}
