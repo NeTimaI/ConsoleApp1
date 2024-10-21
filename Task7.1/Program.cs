04edf8b6dc4bab9ba4391652250d9ed5209640be
Timofey_Kirilichev <Timof@DESKTOP-CCFM2G9>
2024-10-21 16:27:06 +0500

    Задание по методу с функцией if выполненно.

---
+++ Task7.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            var n = double.Parse(Console.ReadLine());
+            if (n % 5 == 0)
+            {
+                Console.WriteLine("Кртано 5");
+            }
+            else if (n % 7 == 0)
+            {
+                Console.WriteLine("Кратно 7");
+            }
+            else 
+            { 
+                Console.WriteLine("Не кратно 7 и 5"); 
+            }
+            Console.ReadKey();
+        }
+    }
+}
