648b901a96e2ec9016b1ce552de1848834ffb73b
Timofey_Kirilichev <Timof@DESKTOP-CCFM2G9>
2024-11-11 16:40:41 +0500

    Задание с условными операторами выполнено. Вариант 10

---
+++ Task 8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение x для функции y=f(x)");
+            var x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"f({x}) = {MyFunction(x)}");
+        }
+            static double MyFunction(double x)
+            {
+                if (x < -1)
+                    return 1;
+                else if (x > 1)
+                    return -1;
+                else 
+                    return (x*(-1));
+            }
+        }
+    }
