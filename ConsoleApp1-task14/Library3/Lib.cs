67a9b7207c1c914d27da28cbf98044530dcbde92
Timofey_Kirilichev <Timof@DESKTOP-CCFM2G9>
2025-06-08 18:52:30 +0500

    Задание 15. Интерфейсы. Вариант 10

---
+++ ConsoleApp1-task14/Library3/Lib.cs
@@ -0,0 +1,29 @@
+﻿using Library3;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Library3
+{
+    public class Library : IEnumerable<Reader>
+    {
+        public string Name { get; }
+        public string Address { get; }
+        public int ReaderCount => _readers.Count;
+        private readonly List<Reader> _readers;
+
+        public Library(string name, string address, IEnumerable<Reader> readers)
+        {
+            Name = name;
+            Address = address;
+            _readers = readers
+                .GroupBy(r => r.CardNumber)
+                .Select(g => g.First())
+                .ToList();
+        }
+    public IEnumerator<Reader> GetEnumerator() => _readers.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    }
+}
