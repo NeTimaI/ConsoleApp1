a35f73a7cc195cc18a74d06280abd2dd2a6b7726
Timofey_Kirilichev <Timof@DESKTOP-CCFM2G9>
2025-06-08 13:52:30 +0500

    Задание 14. Наследование и Полиморфизмы. Вариант 10

---
+++ Library3/RegularReader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library3
+{
+    public class RegularReader : Reader
+    {
+        public DateTime RegistrationDate { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+
+        public RegularReader(string name, string surname, string cardNumber, string issueDate, int loanDays, decimal deposit, string registrationDate)
+            : base(name, surname, cardNumber, issueDate, loanDays, deposit)
+        {
+            if (!DateTime.TryParse(registrationDate, out var parsedDate))
+                throw new ArgumentException("Неверный формат даты записи");
+
+            RegistrationDate = parsedDate;
+        }
+
+        public override string[] GetInfo()
+        {
+            var baseInfo = base.GetInfo();
+            return new[]
+            {
+            baseInfo[0],
+            baseInfo[1],
+            $"Дата записи: {RegistrationDate:d}, Адрес: {Address}, Телефон: {Phone}"
+        };
+        }
+    }
+}
